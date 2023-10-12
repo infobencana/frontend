59466830db1b281b2f79d4946fe4995c68ddf2d5
M Yardan Aqiella <myardan.aqiella22@gmail.com>
2023-10-12 10:58:50 +0700

    update loading animation

--- src/components/ui/loading.jsx
+++ src/components/ui/loading.jsx
@@ -1,13 +1,18 @@
-import Logo from "@/assets/images/circle-logo.webp";
+import Logo from "@/assets/images/circle-logo-filled.svg";
 
 export function Loading() {
   return (
-    <div className="absolute w-screen h-screen bg-white flex justify-center items-center">
+    <div className="absolute w-screen h-[100dvh] bg-white flex justify-center items-center z-20">
       <img
         src={Logo}
         alt="loading logo"
-        className="w-14 h-14 lg:w-20 lg:h-20 animate-pulse animate-infinite animate-delay-500"
+        className="absolute w-20 h-20 lg:w-[100px] lg:h-[100px] z-50"
+        draggable={"false"}
       />
+      <div className="pulse z-30">
+        <div className="z-40"></div>
+        <div className="z-40"></div>
+      </div>
     </div>
   );
 }
--- src/layouts/auth-layout.jsx
+++ src/layouts/auth-layout.jsx
@@ -1,15 +1,10 @@
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-
+import { Link, Navigate, useLocation } from "react-router-dom";
 import { useUser } from "@/context/user-context";
 import { cn } from "@/utils/cn";
-
 import Logo from "@/assets/images/circle-logo.webp";
 import AuthImg from "@/assets/images/auth-img.jpg";
-import { Navigate } from "react-router-dom";
 
 export function AuthLayout({ children }) {
-  const { user } = useUser();
   const { pathname } = useLocation();
   const authType = pathname.replace("/auth/", "");
 
@@ -26,7 +21,7 @@ export function AuthLayout({ children }) {
     },
   };
 
-  if (user) return <Navigate to="/" replace />;
+  if (useUser().user) return <Navigate to="/" replace />;
 
   return (
     <div
@@ -36,20 +31,19 @@ export function AuthLayout({ children }) {
         "lg:min-h-[650px] 2xl:max-w-full",
       )}
     >
-      {/* Left */}
       <div
         className={cn(
           "flex flex-col justify-center items-center",
-          "w-full h-full px-4 py-10 lg:max-w-[550px] 2xl:max-w-[50%]",
+          "w-full h-full px-6 py-10 lg:px-4 lg:max-w-[550px] 2xl:max-w-[50%]",
         )}
       >
         <div className="mx-auto min-w-[300px] max-w-[400px] w-full 2xl:max-w-2xl">
           <img
             src={Logo}
             alt="logo info bencana"
-            className="w-14 h-14 mx-auto"
+            className="w-16 h-16 mx-auto"
           />
-          <div className="text-center mt-5 w-5/6 mx-auto sm:w-full">
+          <div className="text-center mt-5 w-full mx-auto sm:w-full">
             <h1 className="text-black font-semibold text-xl xl:text-2xl">
               Selamat Datang
             </h1>
@@ -71,7 +65,6 @@ export function AuthLayout({ children }) {
           </div>
         </div>
       </div>
-      {/* Right */}
       <div
         className={cn(
           "items-center justify-start",
