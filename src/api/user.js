3429998c3be5cfbd597dbf7e9c9119f08c21ea51
Ahmad Indra Nurfauzi <ahmadindran@gmail.com>
2023-10-18 18:01:19 +0700

    feat:: update profile

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,7 @@ import { client } from "./client";
 export function getCurrentUser() {
   return client.get("/profile");
 }
+
+export function updateProfile(data) {
+  return client.put("/profile", {...data});
+}
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,4 +1,10 @@
+import React, { useState } from "react";
+
 import { useUser } from "@/context/user-context";
+
+import { updateProfile } from "@/api/user";
+
+import { IconTrash, IconCloudUpload } from "@tabler/icons-react";
 
 import { Button } from "../../components/ui/button";
 
@@ -7,13 +13,33 @@ import { useNavigate } from "react-router-dom";
 export default function Profile() {
   const { user } = useUser();
 
-  if (!user) {
-    return <div>Loading...</div>;
-  }
+  const [email, setEmail] = useState(user.email);
+  const [fullName, setFullName] = useState(user.full_name);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleFullNameChange = (e) => {
+    setFullName(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      console.log("Submit data", email, fullName);
+      // data = { email: email, full_name: fullName };
+      await updateProfile({ full_name: fullName });
+      console.log("Profile updated successfully!");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error updating profile:", error);
+    }
+  };
 
   return (
     <>
       <div className="flex">
+        {/* Photo section */}
         <div className="w-2/5 p-4 flex flex-col items-center">
           <img
             src={user.photo_profile}
@@ -23,14 +49,17 @@ export default function Profile() {
 
           <div className="mt-4 w-full">
             <Button className="bg-white text-black font-bold px-4 py-2 rounded mb-2 w-full border">
+              <IconCloudUpload size={26} className="mr-4" />
               Upload Foto
             </Button>
-            <Button className="font-bold rounded-md mt-4 w-full">
+            <Button className="hover:bg-red-500 font-bold rounded-md mt-4 w-full">
+              <IconTrash size={26} className="text-snow mr-4" />
               Hapus Foto
             </Button>
           </div>
         </div>
 
+        {/* user accounts section */}
         <div className="w-3/5 mx-auto p-4 border-l pl-10">
           <div className="mb-4 pb-2 border-b">
             <h1 className="text-2xl font-bold mb-4">Detail Akun</h1>
@@ -41,8 +70,9 @@ export default function Profile() {
               <label className="block font-semibold mb-2">Email</label>
               <input
                 type="email"
-                value={user.email}
-                className="w-full border border-gray-400 rounded px-3 py-2"
+                value={email}
+                onChange={handleEmailChange}
+                className="w-full border border-gray-400 rounded-lg px-3 py-2"
               />
             </div>
 
@@ -50,13 +80,17 @@ export default function Profile() {
               <label className="block font-semibold mb-2">Nama Lengkap</label>
               <input
                 type="text"
-                value={user.full_name}
-                className="w-full border border-gray-400 rounded px-3 py-2"
+                value={fullName}
+                onChange={handleFullNameChange}
+                className="w-full border border-gray-400 rounded-lg px-3 py-2"
               />
             </div>
 
             <div className="mt-4 w-full">
-              <Button className="font-bold px-4 py-2 rounded w-full">
+              <Button
+                className="font-bold px-4 py-2 rounded-lg w-full"
+                onClick={handleSubmit}
+              >
                 Perbarui Akun
               </Button>
             </div>
@@ -75,7 +109,7 @@ export default function Profile() {
                 <input
                   type="email"
                   placeholder="Masukkan Password"
-                  className="w-full border border-gray-400 rounded px-3 py-2"
+                  className="w-full border border-gray-400 rounded-lg px-3 py-2"
                 />
               </div>
 
@@ -86,12 +120,12 @@ export default function Profile() {
                 <input
                   type="text"
                   placeholder="Masukkan Password"
-                  className="w-full border border-gray-400 rounded px-3 py-2"
+                  className="w-full border border-gray-400 rounded-lg px-3 py-2"
                 />
               </div>
 
               <div className="mt-4 w-full">
-                <Button className="font-bold px-4 py-2 rounded w-full">
+                <Button className="font-bold px-4 py-2 rounded-lg w-full">
                   Perbarui Password
                 </Button>
               </div>
