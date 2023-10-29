ace85cbd3207ef31bedc7cd257d3443c48112575
M Yardan Aqiella <myardan.aqiella22@gmail.com>
2023-10-29 17:28:54 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { compression } from "vite-plugin-compression2";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression(),
+    compression({
+      algorithm: "brotliCompress",
+      exclude: [/\.(br)$/, /\.(gz)$/],
+      deleteOriginalAssets: true,
+    }),
+  ],
 });
