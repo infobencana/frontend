5ecde15a6e365e076670107baaa113641d699a0f
M Yardan Aqiella <myardan.aqiella22@gmail.com>
2023-10-30 12:13:43 +0700

    update vite configuration
    change compression config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { compression } from "vite-plugin-compression2";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
@@ -8,5 +9,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      threshold: 512000,
+      algorithm: "brotliCompress",
+      exclude: [/\.(br)$ /, /\.(gz)$/],
+      deleteOriginalAssets: true,
+    }),
+  ],
 });
